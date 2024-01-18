1c1db4d324e54394da46d5341fd1b29199763fad
Ahosall <zen.wixyos.br@gmail.com>
2024-01-18 17:37:20 -0400

    Create structure, Add validation to fields

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+import webview
+import threading
+import watchfiles
+
+class Api:
+  def getConfig(self):
+    config = None
+    try:
+      with open('./config.json', 'r+') as f:
+        config = json.loads(f.read())
+
+      return config
+    except Exception as e:
+      print(e)
+      return None
+    
+  def saveConfig(self, data: dict) -> bool:
+    try:
+      with open('./config.json', 'w+') as f:
+        f.write(json.dumps(data))
+      return True
+    except Exception as e:
+      print(e)
+      return False
+
+def watch_and_reload(window, event):
+  for change in watchfiles.watch('templates/', stop_event=event):
+    window.evaluate_js('window.location.reload()')
+
+if __name__ == '__main__':
+  api = Api()
+  window = webview.create_window(
+    title = '.: OFX-Tools :.',
+    url = './templates/index.html',
+    width=1200,
+    height=650,
+    js_api=api
+  )
+  
+  ## this handles stopping the watcher
+  thread_running = threading.Event()
+  thread_running.set()
+
+  ## using a thread to watch
+  reload_thread = threading.Thread(
+    target = watch_and_reload,
+    args = (window, thread_running)
+  )
+  reload_thread.start()
+  
+  ## start the webview app
+  webview.start(window, debug=True)
+
+  ## upon the webview app exitting, stop the watcher
+  thread_running.clear()
+  reload_thread.join()
+
+  print('exitted successfully!')
